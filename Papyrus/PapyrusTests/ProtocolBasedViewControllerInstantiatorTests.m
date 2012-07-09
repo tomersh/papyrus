dfb879f8b393f9c5701846d2563ff5677cf9a7bc
Tomer Shiri <github@shiri.info>
2012-07-09 21:25:01 +0300

    tests for the base

---
+++ Papyrus/PapyrusTests/ProtocolBasedViewControllerInstantiatorTests.m
@@ -0,0 +1,13 @@
+//
+//  ProtocolBasedViewControllerInstantiatorTests.m
+//  Papyrus
+//
+//  Created by Tomer shiri on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProtocolBasedViewControllerInstantiatorTests.h"
+
+@implementation ProtocolBasedViewControllerInstantiatorTests
+
+@end
