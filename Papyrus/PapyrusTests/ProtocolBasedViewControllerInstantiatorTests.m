c7fb369519c1d205278cfbc9c9d1ff290086dff6
Tomer Shiri <github@shiri.info>
2012-07-15 19:28:44 +0300

    added apache license

--- Papyrus/PapyrusTests/ProtocolBasedViewControllerInstantiatorTests.m
+++ Papyrus/PapyrusTests/ProtocolBasedViewControllerInstantiatorTests.m
@@ -1,10 +1,20 @@
+// Papyrus - Protocol driven framework for automatic loading 
+// and rendering of ViewControllers for iPhone apps.
+// http://github.com/tomersh/papyrus
 //
-//  ProtocolBasedViewControllerInstantiatorTests.m
-//  Papyrus
+//Copyright 2012 Tomer Shiri - papyrus@shiri.info
 //
-//  Created by Tomer shiri on 7/9/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
 //
+//http://www.apache.org/licenses/LICENSE-2.0
+// 
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 
 #import "ProtocolBasedViewControllerInstantiatorTests.h"
 
